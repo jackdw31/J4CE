a5c5e4d937c38505793188faa53b4d86bbde1c93
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-04 18:52:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,38 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const PREFIX = "c.";
+
+var client = new Discord.Client();
+var servers = {};
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("message", function(message) {
+	if (message.author.equals(client.user)) return;
+	
+	if (!message.content.startsWith(PREFIX)) return;
+	
+	var args = message.content.substring(PREFIX.length).split(" ");
+	
+	switch (args[0].toLowerCase()) {
+		case "ping":
+			message.channel.sendMessage(pong!);
+			break;
+		case "bun":
+			message.channel.sendMessage(https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png);
+			break;
+		case "help":
+			var embed = new Discord.RichEmbed()
+				.setTitle("**What can C1NN4-B0T do?**")
+				.addField("c.ping", "Pong!")
+				.setColor(FBA635)
+				.setFooter("The Novelcraft Lounge")
+			message.channel.sendEmbed(embed);
+			break;
+		default:
+			message.channel.sendMessage("Invalid command");
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
