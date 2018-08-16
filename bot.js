019a82573608168e90a716ab1df5719febaf8627
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-16 17:01:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,44 @@
-// setup
+// setup /////////////////////////////////////
 
 const Discord = require('discord.js');
 const PREFIX = "c.";
 var client = new Discord.Client();
-
-// console ready
-
 client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-// command cases
+// help //////////////////////////////////////
 
-client.on("message", function(message) {
-	if (!message.content.startsWith(PREFIX)) return;
-	var args = message.content.substring(PREFIX.length).split(" ");
-	switch (args[0].toLowerCase()) {
-		case "ping":
-			message.channel.sendMessage("Pong!");
-			break;
-		case "bun":
-			const attachment = new Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
-			message.channel.send(attachment);
-			break;
-		case "help":
-			var embed = new Discord.RichEmbed()
-				.setTitle("**What can C1NN4-B0T do?**")
-				.addField("c.help", "Displays list of commands")
-				.addField("c.ping", "Pong!")
-				.addField("c.bun", "Reminds you of your cinnamon bun-ness!")
-				.setColor(0xFBA635)
-				.setFooter("The Novelcraft Lounge")
-			message.channel.sendEmbed(embed);
-			break;
-		default:
-			message.channel.sendMessage("Invalid command!");
+client.on('message', message => {
+	if (message.content === PREFIX + 'help') {
+	var embed = new Discord.RichEmbed()
+		.setTitle("**What can C1NN4-B0T do?**")
+		.addField("c.help", "Displays list of commands")
+		.addField("c.ping", "Pong!")
+		.addField("c.bun", "Reminds you of your cinnamon bun-ness!")
+		.setColor(0xFBA635)
+		.setFooter("The Novelcraft Lounge")
+	message.channel.sendEmbed(embed);
 	}
 });
 
-// token login
+// ping //////////////////////////////////////
+
+client.on('message', message => {
+	if (message.content === PREFIX + 'ping') {
+	message.reply('pong!');
+	}
+});
+
+// bun ///////////////////////////////////////
+
+client.on('message', message => {
+	if (message.content === PREFIX + 'bun') {
+	const attachment = new Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
+		message.channel.send(attachment);
+  	}
+});
+
+// token login ///////////////////////////////
+
 client.login(process.env.BOT_TOKEN);
