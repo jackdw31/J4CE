b3714a7f5711dc399b51d074b392e2544f61a64e
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-16 16:13:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,19 +10,35 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-// commands
+// command cases
 
-client.on('message', message => {
-    if (message.content === PREFIX + 'ping') {
-    	message.reply('pong!');
-  	}
+client.on("message", function(message) {
+	if (message.author.equals(client.user)) return;
+	if (!message.content.startsWith(PREFIX)) return;
+	var args = message.content.substring(PREFIX.length).split(" ");
+	
+	switch (args[0].toLowerCase()) {
+		case "ping":
+			message.channel.sendMessage("Pong!");
+			break;
+		case "bun":
+            const attachment = new Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
+            message.channel.send(attachment);
+			break;
+		case "help":
+			var embed = new Discord.RichEmbed()
+				.setTitle("**What can C1NN4-B0T do?**")
+				.addField("c.help", "Displays list of commands")
+				.addField("c.ping", "Pong!")
+				.addField("c.bun", "Reminds you of your cinnamon bun-ness!")
+				.setColor(0xFBA635)
+				.setFooter("The Novelcraft Lounge")
+			message.channel.sendEmbed(embed);
+			break;
+		default:
+			message.channel.sendMessage("Invalid command!");
+	}
 });
 
-client.on('message', message => {
-    if (message.content === PREFIX + 'bun') {
-    	message.reply('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
+// token login
 client.login(process.env.BOT_TOKEN);
