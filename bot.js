3400ec03b26e6c358be118f70e95fa63e271a316
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-16 17:15:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 // setup /////////////////////////////////////
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const PREFIX = "c.";
 var client = new Discord.Client();
-client.on('ready', () => {
-	console.log('I am ready!');
+client.on("ready", () => {
+	console.log("I am ready!");
 });
 
 // help //////////////////////////////////////
 
-client.on('message', message => {
-	if (message.content === PREFIX + 'help') {
+client.on("message", message => {
+	if (message.content === PREFIX + "help") {
 	var embed = new Discord.RichEmbed()
 		.setTitle("**What can C1NN4-B0T do?**")
 		.addField("**c.help**", "Displays list of commands")
@@ -24,17 +24,17 @@ client.on('message', message => {
 
 // ping //////////////////////////////////////
 
-client.on('message', message => {
-	if (message.content === PREFIX + 'ping') {
-	message.channel.send('pong!');
+client.on("message", message => {
+	if (message.content === PREFIX + "ping") {
+	message.channel.send("pong!");
 	}
 });
 
 // bun ///////////////////////////////////////
 
-client.on('message', message => {
-	if (message.content === PREFIX + 'bun') {
-	const attachment = Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
+client.on("message", message => {
+	if (message.content === PREFIX + "bun") {
+	const attachment = "https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png";
 		message.channel.send(attachment);
   	}
 });
