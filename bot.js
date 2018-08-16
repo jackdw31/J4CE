690065ad5da8c7d2a19ccdaf2480a4df3edd1c1e
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-16 17:30:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,7 @@ client.on("message", message => {
 
 client.on("message", message => {
 	if (message.content === PREFIX + "bun") {
-	const attachment = new Attachment("https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png");
-		message.channel.send(attachment);
+	message.channel.send("https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png");
   	}
 });
 
