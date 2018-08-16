c850d5d8e07ec3f81fd7c178ed69b40171ee0789
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-16 16:23:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,13 @@
 
 const Discord = require('discord.js');
 const PREFIX = "c.";
-
 var client = new Discord.Client();
 var servers = {};
 
+// console ready
+
 client.on('ready', () => {
-    console.log('I am ready!');
+	console.log('I am ready!');
 });
 
 // command cases
@@ -16,14 +17,13 @@ client.on("message", function(message) {
 	if (message.author.equals(client.user)) return;
 	if (!message.content.startsWith(PREFIX)) return;
 	var args = message.content.substring(PREFIX.length).split(" ");
-	
 	switch (args[0].toLowerCase()) {
 		case "ping":
 			message.channel.sendMessage("Pong!");
 			break;
 		case "bun":
-            const attachment = new Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
-            message.channel.send(attachment);
+			const attachment = new Attachment('https://cdn.discordapp.com/attachments/475314976819642368/475360270596964378/unknown.png');
+			message.channel.send(attachment);
 			break;
 		case "help":
 			var embed = new Discord.RichEmbed()
